915b1983f1455e80a1ff4d2608d897f9058c570a
HrRodan <m.mueller89@gmail.com>
2022-02-04 21:47:54 +0100

    day8

--- day08/day8.py
+++ day08/day8.py
@@ -3,19 +3,17 @@ with open('input.txt') as file:
 
 
 def execute_program(program_input: dict):
-    cmd_count = {key: 0 for key in program_input}
+    visited = set()
     program_length = len(program)
 
     def execute_cmd(cmd_key: int = 0, score: int = 0, ):
         # condition for next cmd after last init command
         if cmd_key == program_length:
             return (True, cmd_key, score)
-        elif cmd_key > program_length or cmd_key < 0:
+        elif cmd_key > program_length or cmd_key < 0 or cmd_key in visited:
             return (False, cmd_key, score)
 
-        cmd_count[cmd_key] += 1
-        if any(value >= 2 for value in cmd_count.values()):
-            return (False, cmd_key, score)
+        visited.add(cmd_key)
         cmd, value = program_input[cmd_key]
         if cmd == 'nop':
             return execute_cmd(cmd_key + 1, score)
