e7173bb0f3845292aa19fb09eed9848d0659ec2d
HrRodan <m.mueller89@gmail.com>
2022-02-04 21:57:53 +0100

    day8

--- day08/day8.py
+++ day08/day8.py
@@ -6,21 +6,21 @@ def execute_program(program_input: dict):
     visited = set()
     program_length = len(program)
 
-    def execute_cmd(cmd_key: int = 0, score: int = 0, ):
+    def execute_cmd(cmd_key: int = 0, score_input: int = 0):
         # condition for next cmd after last init command
         if cmd_key == program_length:
-            return (True, cmd_key, score)
+            return (True, cmd_key, score_input)
         elif cmd_key > program_length or cmd_key < 0 or cmd_key in visited:
-            return (False, cmd_key, score)
+            return (False, cmd_key, score_input)
 
         visited.add(cmd_key)
         cmd, value = program_input[cmd_key]
         if cmd == 'nop':
-            return execute_cmd(cmd_key + 1, score)
+            return execute_cmd(cmd_key + 1, score_input)
         elif cmd == 'acc':
-            return execute_cmd(cmd_key + 1, score + value)
+            return execute_cmd(cmd_key + 1, score_input + value)
         elif cmd == 'jmp':
-            return execute_cmd(cmd_key + value, score)
+            return execute_cmd(cmd_key + value, score_input)
 
     return execute_cmd()
 
